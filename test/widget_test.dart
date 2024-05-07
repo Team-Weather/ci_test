d50228ab3ebaa02e2c9a19fad8f727c4ef8651fb
hoogom88 <ziziwhdgus@naver.com>
2024-05-07 16:18:10 +0900

    Test: test, lint, env 모두 되는 버전 일듯

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   test('Counter increments smoke test', () {
-    expect(0, 2);
+    expect(dotenv.env['METEO_BASE_URL'], 'https://api.open-meteo.com/');
   });
 }
